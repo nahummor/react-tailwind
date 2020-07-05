a6e0e4ee6feab674a8118eeb9f4f4c0001a41ec1
nahummor <nahummor@gmail.com>
2020-07-05 16:39:24 +0300

    1

--- src/App.js
+++ src/App.js
@@ -10,43 +10,58 @@ function App() {
    };
 
    return (
-      <div className='flex flex-row'>
+      <div
+         className='flex flex-col justify-around h-screen 
+                    items-center bg-gray-100 sm:flex-row'>
          <Card title='Main title' subtitle='sub title'>
             <div className='flex flex-col'>
                <button
                   id='btn-1'
                   onClick={onClickHandler}
-                  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4'>
+                  className='bg-blue-500 hover:bg-blue-700 text-white 
+                             font-bold py-2 px-4 rounded mt-4 shadow-md'>
                   Button 1
                </button>
 
                <button
                   id='btn-2'
                   onClick={onClickHandler}
-                  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4'>
+                  className='bg-blue-500 hover:bg-blue-700 text-white 
+                             font-bold py-2 px-4 rounded mt-4 shadow-md'>
                   Button 2
                </button>
 
                <button
                   id='btn-3'
                   onClick={onClickHandler}
-                  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4'>
+                  className='bg-blue-500 hover:bg-blue-700 text-white 
+                                  font-bold py-2 px-4 rounded mt-4 shadow-md'>
                   Button 3
                </button>
             </div>
          </Card>
          <Card title='form 1' subtitle='add new data'>
             <label
-               className='block text-gray-700 text-sm font-bold mb-2 text-right'
+               className='block text-gray-700 text-sm 
+                          font-bold mb-2 text-right'
                htmlFor='username'>
                שם משתמש
             </label>
             <input
-               className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+               className='shadow appearance-none border rounded 
+                          w-full py-2 px-3 text-gray-700 leading-tight 
+                          focus:outline-none focus:shadow-outline'
                id='username'
                type='text'
                placeholder='שם משתמש'
             />
+            <button
+               className='bg-indigo-600 hover:bg-indigo-400 
+                          hover:text-gray-800
+                          text-gray-400 font-medium text-lg p-4 mt-2
+                          rounded-md shadow-md w-full'>
+               שמירה
+            </button>
          </Card>
       </div>
    );
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,12 +4,11 @@ const Card = ({ title, subtitle, children }) => {
    return (
       <div
          dir='rtl'
-         className='container mx-auto flex flex-row justify-center h-screen items-center'>
-         <div className='text-center bg-gray-300 h-64 rounded shadow-2xl border-solid border-2 border-gray-600 w-56 p-2'>
-            <p className='text-blue-300 text-lg font-bold'>{title}</p>
-            <p className='text-black-300 text-lg'>{subtitle}</p>
-            {children}
-         </div>
+         className='text-center bg-gray-300 h-64 rounded shadow-2xl
+                    border-solid border-2 border-gray-600 w-90 p-2'>
+         <p className='text-blue-400 text-lg font-bold'>{title}</p>
+         <p className='text-gray-800 text-lg font-light'>{subtitle}</p>
+         {children}
       </div>
    );
 };
