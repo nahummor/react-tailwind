cd8f7a4b89e01fd5c16f0e6fe555e415e15cce4a
nahummor <nahummor@gmail.com>
2020-07-06 06:44:33 +0300

    2

--- src/App.js
+++ src/App.js
@@ -11,14 +11,15 @@ function App() {
 
    return (
       <div
+         dir='rtl'
          className='flex flex-col justify-around h-screen 
                     items-center bg-gray-100 sm:flex-row'>
-         <Card title='Main title' subtitle='sub title'>
+         <Card title='Main Title' subtitle='sub title'>
             <div className='flex flex-col'>
                <button
                   id='btn-1'
                   onClick={onClickHandler}
-                  className='bg-blue-500 hover:bg-blue-700 text-white 
+                  className='bg-facebook-blue hover:bg-blue-700 text-white 
                              font-bold py-2 px-4 rounded mt-4 shadow-md'>
                   Button 1
                </button>
@@ -26,7 +27,7 @@ function App() {
                <button
                   id='btn-2'
                   onClick={onClickHandler}
-                  className='bg-blue-500 hover:bg-blue-700 text-white 
+                  className='bg-ios-green hover:bg-blue-700 text-white 
                              font-bold py-2 px-4 rounded mt-4 shadow-md'>
                   Button 2
                </button>
@@ -34,34 +35,36 @@ function App() {
                <button
                   id='btn-3'
                   onClick={onClickHandler}
-                  className='bg-blue-500 hover:bg-blue-700 text-white 
-                                  font-bold py-2 px-4 rounded mt-4 shadow-md'>
+                  className='app-button'>
                   Button 3
                </button>
             </div>
          </Card>
-         <Card title='form 1' subtitle='add new data'>
-            <label
-               className='block text-gray-700 text-sm 
-                          font-bold mb-2 text-right'
-               htmlFor='username'>
-               שם משתמש
+         <Card title='פרטי עובד' subtitle='add new data'>
+            <label className='form-label' htmlFor='firstName'>
+               שם פרטי
             </label>
             <input
                className='shadow appearance-none border rounded 
-                          w-full py-2 px-3 text-gray-700 leading-tight 
+                          w-full py-2 px-3 mb-2 text-gray-700 leading-tight 
                           focus:outline-none focus:shadow-outline'
-               id='username'
+               id='firstName'
                type='text'
-               placeholder='שם משתמש'
+               placeholder='שם פרטי'
             />
-            <button
-               className='bg-indigo-600 hover:bg-indigo-400 
-                          hover:text-gray-800
-                          text-gray-400 font-medium text-lg p-4 mt-2
-                          rounded-md shadow-md w-full'>
-               שמירה
-            </button>
+
+            <label className='form-label' htmlFor='lastName'>
+               שם משפחה
+            </label>
+            <input
+               className='shadow appearance-none border rounded
+                          w-full py-2 px-3 mb-2 text-gray-700 leading-tight 
+                          focus:outline-none focus:shadow-outline'
+               id='lastName'
+               type='text'
+               placeholder='שם משפחה'
+            />
+            <button className='app-button'>שמירה</button>
          </Card>
       </div>
    );
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,9 +4,10 @@ const Card = ({ title, subtitle, children }) => {
    return (
       <div
          dir='rtl'
-         className='text-center bg-gray-300 h-64 rounded shadow-2xl
-                    border-solid border-2 border-gray-600 w-90 p-2'>
-         <p className='text-blue-400 text-lg font-bold'>{title}</p>
+         className='text-center bg-gray-300 rounded shadow-2xl 
+                    w-5/6 sm:w-1/3 md:w-1/4
+                    border-solid border-2 border-gray-600 p-2'>
+         <p className='text-blue-400 text-lg font-bold text-right'>{title}</p>
          <p className='text-gray-800 text-lg font-light'>{subtitle}</p>
          {children}
       </div>
